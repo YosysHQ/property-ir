70ef717558eff66a60d64a6a9eba4780e88494ee
Lena Schiffer <lena@yosyshq.com>
2026-01-14 14:45:55 +0900

    remove union by rank from UnionFind

--- prototype/src/sexpr/utils.py
+++ prototype/src/sexpr/utils.py
@@ -10,11 +10,9 @@ class UnionFind[T]:
     data structure when they get merged."""
 
     parents: dict[T, T]
-    ranks: dict[T, int]
 
     def __init__(self):
         self.parents =  dict()
-        self.ranks = dict()
 
     def find(self, elem: T) -> T:
 
@@ -34,22 +32,13 @@ class UnionFind[T]:
 
         if elem1 not in self.parents:
             self.parents[elem1] = elem1
-            self.ranks[elem1] = 0
         if elem2 not in self.parents:
             self.parents[elem2] = elem2
-            self.ranks[elem2] = 0
 
         repr1: T = self.find(elem1)
         repr2: T = self.find(elem2)
 
-        # union by rank
-        if self.ranks[repr1] < self.ranks[repr2]:
-            self.parents[repr1] = repr2
-        elif self.ranks[repr2] < self.ranks[repr1]:
-            self.parents[repr2] = repr1
-        else:
-            self.parents[repr1] = repr2
-            self.ranks[repr2] += 1
+        self.parents[repr1] = repr2
 
     def make_representative(self, elem: T):
         current_repr = self.find(elem)
