5a44631ca2800d7ab2208cd95e3cb32ad49598d1
Lena Schiffer <lena@yosyshq.com>
2026-04-10 11:55:36 +0900

    add primitives for simple sequences and properties

--- prototype/src/sexpr/primitives.py
+++ prototype/src/sexpr/primitives.py
@@ -28,10 +28,31 @@ class And(Bool):
 class Or(Bool):
     children: list[NodeId[Bool]]
 
+@typechecked
+@dataclass
+class Xor(Bool):
+    child1: NodeId[Bool]
+    child2: NodeId[Bool]
 
+@typechecked
+@dataclass
+class Eq(Bool):
+    child1: NodeId[Bool]
+    child2: NodeId[Bool]
 
 
 # Sequence primitives
+
+@typechecked
+@dataclass
+class SeqBool(Sequence):
+    child: NodeId[Bool]
+
+@typechecked
+@dataclass
+class SeqRepeat(Sequence):
+    child1: Range
+    child2: NodeId[Sequence]
 
 @typechecked
 @dataclass
@@ -40,23 +61,114 @@ class SeqConcat(Sequence):
 
 @typechecked
 @dataclass
-class SeqBool(Sequence):
-    child: Bool
+class SeqFusion(Sequence):
+    children: list[NodeId[Sequence]]
 
 @typechecked
 @dataclass
-class SeqRepeat(Sequence):
-    child1: Range
-    child2: NodeId[Sequence]
+class SeqOr(Sequence):
+    children: list[NodeId[Sequence]]
+
+@typechecked
+@dataclass
+class SeqIntersect(Sequence):
+    children: list[NodeId[Sequence]]
+
+@typechecked
+@dataclass
+class SeqFirstMatch(Sequence):
+    child: NodeId[Sequence]
+
 
 
 
 # Property primitives
 
+# non-derived primitives
+
 @typechecked
 @dataclass
 class PropBool(Property):
     child: NodeId[Bool]
+
+@typechecked
+@dataclass
+class PropSeq(Property):
+    child: NodeId[Sequence]
+
+@typechecked
+@dataclass
+class PropStrong(Property):
+    child: NodeId[Sequence]
+
+@typechecked
+@dataclass
+class PropWeak(Property):
+    child: NodeId[Sequence]
+
+@typechecked
+@dataclass
+class PropNot(Property):
+    child: NodeId[Property]
+
+@typechecked
+@dataclass
+class PropNexttime(Property):
+    child1: int
+    child2: NodeId[Property]
+
+@typechecked
+@dataclass
+class PropOverlappedImplication(Property):
+    child1: NodeId[Sequence]
+    child2: NodeId[Property]
+
+@typechecked
+@dataclass
+class PropUntil(Property):
+    child1: NodeId[Property]
+    child2: NodeId[Property]
+
+@typechecked
+@dataclass
+class PropAcceptOn(Property):
+    child1: NodeId[Bool]
+    child2: NodeId[Property]
+
+# derived but dual primitives (needed for NNF)
+
+@typechecked
+@dataclass
+class PropStrongNexttime(Property):
+    child1: int
+    child2: NodeId[Property]
+
+@typechecked
+@dataclass
+class PropOverlappedFollowedBy(Property):
+    child1: NodeId[Sequence]
+    child2: NodeId[Property]
+
+@typechecked
+@dataclass
+class PropRejectOn(Property):
+    child1: NodeId[Bool]
+    child2: NodeId[Property]
+
+@typechecked
+@dataclass
+class PropStrongUntilWith(Property):
+    child1: NodeId[Property]
+    child2: NodeId[Property]
+
+
+# the following are probably not needed for simple properties
+
+@typechecked
+@dataclass
+class PropNonOverlappedImplication(Property):
+    child1: NodeId[Sequence]
+    child2: NodeId[Property]
 
 @typechecked
 @dataclass
@@ -73,22 +185,3 @@ class PropAlwaysRanged(Property):
 @dataclass
 class PropAnd(Property):
     children: list[NodeId[Property]]
-
-@typechecked
-@dataclass
-class PropSeq(Property):
-    child: NodeId[Sequence]
-
-@typechecked
-@dataclass
-class PropNonOverlappedImplication(Property):
-    child1: NodeId[Sequence]
-    child2: NodeId[Property]
-
-@typechecked
-@dataclass
-class PropOverlappedImplication(Property):
-    child1: NodeId[Sequence]
-    child2: NodeId[Property]
-
-
